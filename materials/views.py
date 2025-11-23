71a831a7dbe11b1837b7230f92d8291c61b1d784
LimDmitriy <160336426+LimDmitriy@users.noreply.github.com>
2025-11-23 23:58:45 +0900

    Созданы CRUD для моделей Course и Lesson, с помощью generic и ViewSet

--- materials/views.py
+++ materials/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from rest_framework.generics import (CreateAPIView, DestroyAPIView,
+                                     ListAPIView, RetrieveAPIView,
+                                     UpdateAPIView)
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from materials.models import Course, Lesson
+from materials.serializers import CourseSerializer, LessonSerializer
+
+
+class CourseViewSet(ModelViewSet):
+    serializer_class = CourseSerializer
+    queryset = Course.objects.all()
+
+
+class LessonCreateApiView(CreateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonListApiView(ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonRetriveApiView(RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonUpdateApiView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class LessonDestroyApiView(DestroyAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
