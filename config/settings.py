09f68cad909db4cae96361a050072ba1c45f4b73
LimDmitriy <160336426+LimDmitriy@users.noreply.github.com>
2025-12-27 05:19:54 +0900

    С помощью celery-beat реализована фоновая задача, которая проверяет пользователей по дате последнего входа

--- config/settings.py
+++ config/settings.py
@@ -133,6 +133,8 @@ CELERY_TASK_TRACK_STARTED = True
 # Максимальное время на выполнение задачи
 CELERY_TASK_TIME_LIMIT = 30 * 60
 
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.yandex.ru"
 EMAIL_PORT = 465
--- materials/tasks.py
+++ materials/tasks.py
@@ -1,12 +1,18 @@
+import time
+from datetime import timedelta
+
 from celery import shared_task
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.core.mail import send_mail
+from django.utils import timezone
 
 from materials.models import Subscription
 
 
 @shared_task
 def send_course_update_email(course_id):
+    """Отправляет сообщения при обновлении курсов или уроков"""
     subscriptions = Subscription.objects.filter(course_id=course_id)
 
     emails = [
@@ -24,3 +30,14 @@ def send_course_update_email(course_id):
         recipient_list=emails,
         fail_silently=False,
     )
+
+User = get_user_model()
+
+@shared_task
+def deactivate_users():
+    """Блокирует пользователей не заходивших более 30 дней"""
+    month_ago = timezone.now() - timedelta(days=30)
+
+    User.objects.filter(last_login__lt=month_ago, is_active=True,).update(is_active=False)
+
+
